306f640bc9e000b74dbc69c7fc656e82a9177501
FloDoyer <94164148+FloDoyer@users.noreply.github.com>
2023-04-14 12:01:23 +0200

    Add files via upload

--- Tag.php
+++ Tag.php
@@ -4,16 +4,16 @@ namespace classe;
 
 class Tag extends Base
 {
-    private string $name;
+    public string $name;
     public int $num_tag;
+    public $alltag = array();
 
-    public function __construct()
+    public function __construct(string $nom_tag, int $num_tag)
     {
         parent::__construct();
-        $tab_tag = $this->getAllTag();
-        $this->name = $tab_tag->nom_tag;
-        $this->num_tag = $tab_tag->pk_num_taf;
-
+        $this->name = $nom_tag;
+        $this->num_tag = $num_tag;
+        $this->init_alltag();
     }
 
     public function getAllTag(): array
@@ -27,6 +27,15 @@ class Tag extends Base
         // Récupération de la réponse sous forme d'un tableau d'instances de GameRenderer
         $results = $statement->fetchAll(PDO::FETCH_CLASS, "BaseRenderer");
         return $results;
+    }
+
+    public function init_alltag():void
+    {
+        $tab_tag = $this->getAllTag();
+        $i = 0;
+        foreach($tab_tag as $T){
+            $this->alltag[$i] = new Tag($T->nom_rec, $T->pk_num_rec );
+        }
     }
 
     //permet d'ajouter des tags
@@ -44,7 +53,7 @@ class Tag extends Base
     public function deleteTag($name)
     {
         $name = htmlspecialchars($name);
-        $del_tag_rec = 'DELETE FROM tag WHERE pk_num_tag = (SELECT pk_num_tag FROM recette WHERE nom_tag = $name)';
+        $del_tag_rec = 'DELETE FROM tag WHERE pk_num_tag = (SELECT pk_num_tag FROM recette WHERE nom_tag = :name)';
         $params = [
             'name' => htmlspecialchars($name)
         ];
@@ -52,11 +61,11 @@ class Tag extends Base
 
     }
 
-  
+    //permet de supprimer toute les elements de tag_recette qui on pour tag name
     public function deleteRecTag($name)
     {
         $name = htmlspecialchars($name);
-        $del_rec_tag = 'DELETE FROM tag_recette WHERE fk_num_tag = (SELECT pk_num_tag FROM tag WHERE nom_rec = $name)';
+        $del_rec_tag = 'DELETE FROM tag_recette WHERE fk_num_tag = (SELECT pk_num_tag FROM tag WHERE nom_rec = :name)';
         $params = [
             'name' => htmlspecialchars($name)
         ];
