f116652383738d24165e7cf9f0177c1dc1995cd1
ValentinD62 <valdu62570pihem@gmail.com>
2023-04-14 13:37:57 +0200

    rech

---
+++ class/recherche/Recherche.php
@@ -0,0 +1,20 @@
+<?php
+namespace recherche;
+
+use recette\RecetteBD;
+
+class Recherche{
+    function getRechercheRecette(): void{
+        $recherche = $_POST['recherche'];
+        if ($recherche == ""){
+            $recettes = new RecetteBD();
+            $liste_recette = $recettes->getAllRecette(); ?>
+            <h1>GAMES</h1>
+            <section class = "recettes-list"><!--Affichage du champ 'name' des objets récupérés -->
+            <?php foreach ($liste_recette as $recette): ?>
+                <?= $recette->getHTML() ?>
+            <?php endforeach; ?>
+            </section> <?php
+        }
+    }
+}
