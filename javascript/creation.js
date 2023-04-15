a0adb99110cdc159c2046991eeaac81ea5e5cd4f
Shaddushi <94164129+Shaddushi@users.noreply.github.com>
2023-04-15 21:33:27 +0200

    fixed add_ingredient

--- javascript/creation.js
+++ javascript/creation.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', function (){
     // récupération du formulaire
     let form_tag = document.getElementById("form_tag");
@@ -33,55 +32,46 @@ document.addEventListener('DOMContentLoaded', function (){
         let inputs = form_ingredient.getElementsByTagName("input"); //Liste d'input.
         form_ingredient.addEventListener('keyup', function(){
             let i = 0;
+            let name = [];
             for (let input of inputs){
                 if (input.type === "text"){
                     i = i+1;
+                    name[i] = input;
+                    console.log(name[i].value)
                 }
             }
-            console.log(i);
-            if (inputs[i-1].type === "text"){
-                if (inputs[i-1].value.length >= 1 && inputs[i] !== null){
-                    let vrai_i = i+1;
-                    let inp = document.createElement("input");
-                    inp.type = "text";
-                    inp.classList.add("form-ingredient-input");
-                    inp.id = "ingredient_" + vrai_i;
-                    inp.color = "black";
-                    inp.name = "ingredient_" + vrai_i;
-                    form_ingredient.appendChild(inp);
-                    let img_add = document.createElement("input");
-                    img_add.type = "file";
-                    img_add.classList.add("img_ing_add");
-                    img_add.name = "image_ing_" + vrai_i;
-                    img_add.accept = "image/png, image/gif, image/jpeg";
-                    form_ingredient.appendChild(img_add);
-                    console.log("If : " + inputs[i-1].type);
+            let j = 0;
+            let image = [];
+            for (let input of inputs){
+                if (input.type === "file"){
+                    j = j+1;
+                    image[i] = input;
                 }
             }
-            else{
-                console.log("else");
-                if (inputs[i-2].value.length >= 1 && inputs[i] !== null){
-                    console.log("Dans le else : " + inputs[i-2].id);
-                    let vrai_i = i+1;
-                    let inp = document.createElement("input");
-                    inp.type = "text";
-                    inp.classList.add("form-ingredient-input");
-                    inp.id = "ingredient_" + vrai_i;
-                    inp.color = "black";
-                    inp.name = "ingredient_" + vrai_i;
-                    form_ingredient.appendChild(inp);
-                    let img_add = document.createElement("input");
-                    img_add.type = "file";
-                    img_add.classList.add("img_ing_add");
-                    img_add.name = "image_ing_" + vrai_i;
-                    img_add.accept = "image/png, image/gif, image/jpeg";
-                    form_ingredient.appendChild(img_add);
-                }
+            if (name[i].value.length >= 1){
+                let vrai_i = i+1;
+                let inp = document.createElement("input");
+                inp.type = "text";
+                inp.classList.add("form-ingredient-input");
+                inp.id = "ingredient_" + vrai_i;
+                inp.color = "black";
+                inp.name = "ingredient_" + vrai_i;
+                form_ingredient.appendChild(inp);
+                let img_add = document.createElement("input");
+                img_add.type = "file";
+                img_add.classList.add("img_ing_add");
+                img_add.name = "image_ing_" + vrai_i;
+                img_add.accept = "image/png, image/gif, image/jpeg";
+                form_ingredient.appendChild(img_add);
+                console.log("If : " + inputs[i-1].type);
             }
+
             if (i >= 3) {
-                if (inputs[i-3].value.length === 0){
-                    form_ingredient.removeChild(inputs[i-1])
-                    form_ingredient.removeChild(inputs[i-1])
+                console.log(name[i].value.length)
+                if (name[i].value.length == 0 && name[i-1].value.length == 0){
+
+                    form_ingredient.removeChild(name[i])
+                    form_ingredient.removeChild(image[i])
                 }
             }
         })
