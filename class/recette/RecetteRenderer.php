ea30d2cba2f405524f6a1267316ba591cba6f213
ValentinD62 <valdu62570pihem@gmail.com>
2023-05-28 19:04:22 +0200

    début de la fonction pour la modif mais j'ai pas le temps hahahaha

--- class/recette/RecetteRenderer.php
+++ class/recette/RecetteRenderer.php
@@ -77,4 +77,54 @@ class RecetteRenderer{
             <?php endif ?>
         </div>
     <?php }
+
+    //Fonction pour pouvoir modifier les différentes recettes.
+    public function getAllModifHTML($recette): void{ ?>
+        <form id="edit-form" method="POST" enctype="multipart/form-data">
+            <div id="detail-Title">
+                <label for="nom_recette"> Nom de la Recette</label>
+                <input type="text" class = "input-modif" id = "nom_recette" name = "nom_recette" value="<?= $recette->name?>">
+            </div>
+
+            <div id="detail">
+                <div id = "detail-description">
+                    <label for="nom_description"> Description</label>
+                    <textarea class = "input-modif" id = "nom_description" name = "nom_description"> <?= $recette->description?> </textarea>
+                </div>
+                <div id="detail-img">
+                    <label for="image_recette"> Image :</label>
+                    <input type="file" class = "input-modif" id = "image_recette" name = "image_recette" value="<?= "../img/" . $recette->image?>" accept="image/png, image/gif, image/jpeg">
+                    <img src= "../img/<?= $recette->image?>" alt="image de la recette">
+                </div>
+            </div>
+        </form>
+
+        <div id = "detail-tag-ing">
+            <div class = "detail-tag">
+                <div class="txt-detail"> Tag : </div>
+                <?php
+                if ($recette->liste_tag != null){
+                    foreach ($recette->liste_tag as $tag){ ?>
+                        <div class = "tag_recette">
+                            <input type="text" class = "input-modif" id = "nom_recette" name = "nom_recette" value="<?= $recette->name?>">
+                        </div>
+                        <?php
+                    }
+                }?>
+            </div>
+            <?php if ($recette->liste_ing != null): ?>
+                <div class="detail-ing">
+                    <div class="txt-detail">Ingredients : </div>
+
+                    <?php foreach ($recette->liste_ing as $ing): ?>
+                        <div class = "sep-detail-ing">
+                            <img src= "../img/img_ingredient/<?=$ing[1]?>">
+                            <div class = "ing_recette"><?= $ing[0]?></div>
+                        </div>
+                    <?php endforeach; ?>
+
+                </div>
+            <?php endif ?>
+        </div>
+    <?php }
 }
--- pages/edit-recette.php
+++ pages/edit-recette.php
@@ -1,14 +1,24 @@
 <?php require_once __DIR__ . "/../class/autoload.php";
 Autoloader::register();
+
+use recette\Recette;
+use recette\RecetteBD;
+use recette\RecetteRenderer;
 use Template\Template;
 
 ob_start()  ?>
-<?php session_start() ;
-session_destroy(); ?>
+
+<?php session_start();?>
 
 <?php
 $id_recette = $_GET['id'];
+$recette_R = new RecetteRenderer();
+$recettesBD = new RecetteBD();
+$recette = new Recette();
+$recettesBD = $recettesBD->getRecetteById($id_recette);
+$liste_recette = $recette->Convertir_recette($recettesBD);
 
+$recette_R->getAllModifHTML($liste_recette[0]);
 
 ?>
 
